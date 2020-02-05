8b3c80bce97b1016e72797f6a98598c9c66737b8
Renato Babata <37925829+renatobabata@users.noreply.github.com>
2020-02-05 13:00:20 -0300

    5. Adding REST GET Controller - completed
    Spring Boot : Complete guide from development to deployment
    5. Adding REST GET Controller

---
+++ restendpointget/src/main/java/com/example/demo/Controller/RestEndpoints.java
@@ -0,0 +1,18 @@
+package com.example.demo.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class RestEndpoints {
+
+    @RequestMapping("/course")
+    public Course getEndpoint(@RequestParam(value="name", defaultValue = "Spring Boot", required = false) String name,
+                              @RequestParam(value="chapterCount", defaultValue = "2", required = false) int chapterCount ){
+        return new Course(name,chapterCount);
+
+    }
+
+}
