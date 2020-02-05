a1f6c25bfc5a6fd506fabe0030dd701efbea9b43
Renato Babata <babatarenato@gmail.com>
2020-02-05 19:43:44 -0300

    Spring Boot - REST API

--- restendpointget/src/main/java/com/example/demo/Controller/RestEndpoints.java
+++ restendpointget/src/main/java/com/example/demo/Controller/RestEndpoints.java
@@ -1,8 +1,6 @@
 package com.example.demo.Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -12,7 +10,10 @@ public class RestEndpoints {
     public Course getEndpoint(@RequestParam(value="name", defaultValue = "Spring Boot", required = false) String name,
                               @RequestParam(value="chapterCount", defaultValue = "2", required = false) int chapterCount ){
         return new Course(name,chapterCount);
-
     }
 
+    @RequestMapping(method = RequestMethod.POST, value = "/register/course")
+    public String saveCourse(@RequestBody Course course){
+        return "Your course named "+ course.getName() + " with " + course.getChapterCount() + " chapters saved successfuly.";
+    }
 }
